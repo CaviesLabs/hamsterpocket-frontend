d5d6c82f5fbdc69c1cb0894f9392110a42fd45bc
lone wolf <leviethung2403@gmail.com>
2023-02-21 19:25:35 -0800

    Render mock data for active pock page

--- src/services/pool.service.ts
+++ src/services/pool.service.ts
@@ -1,3 +1,22 @@
-export class PoolService {}
+import { networkProvider } from "@/src/providers/network.provider";
+import { ActivePocketsMock } from "@/src/services/mock.service";
+import { GetPocketsDto } from "@/src/dto/pocket.dto";
+import { PocketEntity } from "@/src/entities/pocket.entity";
+
+export class PoolService {
+  public async getPockets(payload: GetPocketsDto): Promise<PocketEntity[]> {
+    // TODO remove this line below after developing
+    return ActivePocketsMock;
+
+    return networkProvider
+      .request<PocketEntity[]>(`/pool`, {
+        method: "GET",
+        params: payload,
+      })
+      .catch(() => {
+        return ActivePocketsMock;
+      });
+  }
+}
 
 export const poolService = new PoolService();
