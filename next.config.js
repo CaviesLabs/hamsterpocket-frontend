7b7378be50c0474f862ddf88e8f4edf45323b62b
systemis <cuthinh2002@icloud.com>
2023-03-02 16:50:57 +0700

    chore: Increase reliability of GitHub Actions
    - Add logging to `next.config.js` to log the `API_URL` environment variable
    - Update project dependencies
    
    [next.config.js]
    - Add a `console.log` to the `rewrites` function to log the `API_URL` environment variable

--- next.config.js
+++ next.config.js
@@ -39,6 +39,7 @@ module.exports = withPWA(
       SOLANA_CLUSTER: process.env.SOLANA_CLUSTER,
     },
     async rewrites() {
+      console.log(process.env.API_URL);
       return [
         {
           source: "/api/:path*",
