fb6cd36cf4f33cfdb9b7dc3afa8047739b481761
systemis <cuthinh2002@icloud.com>
2023-03-02 16:22:07 +0700

    chore: Configure multiple locales for Hamsterbox sites
    - Add linting before building the project
    - Support multiple locales: "en-US", "es", "fr", "nl-NL"
    - Set "en-US" as the default locale
    - Set "es" as the default locale for "dev-pocket-fe.hamsterbox.com" and "dev-pocket.hamsterbox.com"
    
    [Dockerfile]
    - Add linting before building the project
    - Remove newline at the end of the file
    [next.config.js]
    - Add support for multiple locales: "en-US", "es", "fr", "nl-NL"
    - Set "en-US" as the default locale
    - Set "es" as the default locale for "dev-pocket-fe.hamsterbox.com"
    - Set "es" as the default locale for "dev-pocket.hamsterbox.com"

--- next.config.js
+++ next.config.js
@@ -54,4 +54,28 @@ module.exports = withPlugins([withPWA, withTM], {
   devIndicators: {
     buildActivity: false,
   },
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en-US", "es", "fr", "nl-NL"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "en-US",
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    domains: [
+      {
+        domain: "dev-pocket-fe.hamsterbox.com",
+        defaultLocale: "en-US",
+        // other locales that should be handled on this domain
+        locales: ["es"],
+      },
+      {
+        domain: "dev-pocket.hamsterbox.com",
+        defaultLocale: "en-US",
+        // other locales that should be handled on this domain
+        locales: ["es"],
+      },
+    ],
+  },
 });
