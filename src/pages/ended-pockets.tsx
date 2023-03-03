43229d5e727e53b123f5a0b9e964724d62aafe01
lone wolf <leviethung2403@gmail.com>
2023-03-03 05:25:51 -0800

    DEV: closed pocket page

---
+++ src/pages/ended-pockets.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import type { NextPage } from "next";
+import MainLayout from "@/src/layouts/main";
+import styles from "@/styles/Home.module.css";
+import { DashboardPageProvider } from "@/src/hooks/pages/dashboard";
+import { useMain } from "@/src/hooks/pages/main";
+import { LayoutSection } from "@/src/components/layout-section";
+import {
+  BalanceGroup,
+  EndedPoolGroupComponent,
+} from "@/src/components/my-pools";
+import { ClosedPockets } from "@/src/components/my-pools/ended-pockets";
+
+const Layout: FC = () => {
+  const {} = useMain();
+
+  return (
+    <MainLayout>
+      <div className={styles.container}>
+        <LayoutSection className="!pt-[160px] pb-[100px]">
+          <BalanceGroup />
+          <EndedPoolGroupComponent />
+          <ClosedPockets />
+        </LayoutSection>
+      </div>
+    </MainLayout>
+  );
+};
+
+const Home: NextPage = () => {
+  return (
+    <DashboardPageProvider>
+      <Layout />
+    </DashboardPageProvider>
+  );
+};
+
+export default Home;
