db0e8aa10457d8ea282ee6ed197ab7bff9a009af
systemis <cuthinh2002@icloud.com>
2023-03-08 19:46:55 +0700

    chore: Improve rounding of pocket balances
    - Round total SOL amount to two decimal places
    - Round total USD amount to five decimal places
    
    [src/components/my-pools/pocket-balance.component.tsx]
    - Change the total SOL amount to be rounded to two decimal places
    - Change the total USD amount to be rounded to five decimal places

--- src/components/my-pools/pocket-balance.component.tsx
+++ src/components/my-pools/pocket-balance.component.tsx
@@ -38,10 +38,10 @@ export const PocketBalanceComponent: FC = () => {
   return (
     <div>
       <p className="text-center text-green normal-text text-[20px]">
-        {totalSOL} SOL
+        {totalSOL.toFixed(2)} SOL
       </p>
       <p className="text-center text-green normal-text text-[16px]">
-        ~${totalUSD}
+        ~${totalUSD.toFixed(5)}
       </p>
       <p className="text-center text-dark40 normal-text text-[14px]">
         Est Pocket balance
