51e0707a967f8b17c323aaa9eadf99128769eacc
hleemors <cuthinh2002@icloud.com>
2023-06-29 17:08:22 +0700

    fix: connector

--- src/hooks/usePlatformConfig.tsx
+++ src/hooks/usePlatformConfig.tsx
@@ -116,14 +116,14 @@ export const PlatformConfigProvider: FC<{ children: ReactNode }> = (props) => {
 
   /** @dev The function to desire chainId in case user do not force chainId by URL. */
   useEffect(() => {
-    if (!desiredChainId) {
+    if (!desiredChainId && !router.query?.chainId) {
       if (window.localStorage.getItem("chainId")) {
         setDesiredChainId(window.localStorage.getItem("chainId"));
       } else {
         setDesiredChainId(ChainId.bnb);
       }
     }
-  }, [desiredChainId]);
+  }, [router]);
 
   return (
     <PlatformConfigContext.Provider
