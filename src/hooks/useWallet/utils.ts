4aec32808e456da6a9c359417f48de79ff772cf5
0xtrou <khangtran.btc@gmail.com>
2023-07-11 20:16:30 +0700

    chore: fixed deps

--- src/hooks/useWallet/utils.ts
+++ src/hooks/useWallet/utils.ts
@@ -1,20 +1,18 @@
 import { DefaultWalletType } from "@saberhq/use-solana";
 import type { WalletName } from "@solana/wallet-adapter-base";
-import {
-  PhantomWalletAdapter,
-  BraveWalletAdapter,
-  Coin98WalletAdapter,
-  LedgerWalletAdapter,
-  SolflareWalletAdapter,
-  SolletWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { BraveWalletAdapter } from "@solana/wallet-adapter-brave";
+import { Coin98WalletAdapter } from "@solana/wallet-adapter-coin98";
+import { LedgerWalletAdapter } from "@solana/wallet-adapter-ledger";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
+import { SolletWalletAdapter } from "@solana/wallet-adapter-sollet";
 
 /** @dev The function to get wallet name. */
 export const getWalletName = (name: string): WalletName => {
   switch (name) {
     case DefaultWalletType.Phantom:
       return new PhantomWalletAdapter().name as WalletName;
-    case DefaultWalletType.BraveWallet:
+    case "Brave":
       return new BraveWalletAdapter().name as WalletName;
     case DefaultWalletType.Coin98:
       return new Coin98WalletAdapter().name as WalletName;
